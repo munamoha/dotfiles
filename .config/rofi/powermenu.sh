d977dacac6b28522df0aee86d317e63dfac70ab2
Muna Mohamed <92002717+munamoha@users.noreply.github.com>
2025-03-11 18:07:26 -0400

    includes edited version of DarkBlue theme to match other items

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,6 @@
+rofi \
+  -show p \
+  -modi p:'rofi-power-menu --dry-run --symbols-font "Symbols Nerd Font Mono"' \
+  -font "JetBrains Mono NF 16" \
+  -theme Paper \
+  -theme-str 'window {width: 8em;} listview {lines: 6;}'
