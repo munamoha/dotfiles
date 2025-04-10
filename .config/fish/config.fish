44ea9d00f871b8e6d4a96d78a76421d1401088ca
Muna Mohamed <92002717+munamoha@users.noreply.github.com>
2025-04-10 11:42:54 -0400

    attempt to set vivaldi as browser

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ zoxide init fish | source
 # Created by `pipx` on 2024-05-08 05:08:50
 set PATH $PATH $HOME/.local/bin
 set -gx EDITOR micro
+set -gx BROWSER vivaldi
 
 set -x PATH $HOME/.pyenv/bin $PATH
 status --is-interactive; and pyenv init --path | source
